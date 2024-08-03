2cfafb5632fb5db4a42c95b3abe5a2f879d86a0d
Pedro Gonçalves <p.goncalves2002@gmail.com>
2024-08-03 19:32:19 -0300

    Initial Commit

---
+++ CapFun/CapFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapFunApp.swift
+//  CapFun
+//
+//  Created by Pedro Gonçalves on 03/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CapFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CapFun/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CapFun
+//
+//  Created by Pedro Gonçalves on 03/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
